6fc97f73dde49c9621244b5f1e2fb9f36e152eb5
Ham Mim <hammim.in@Hams-MacBook-Pro.local>
2025-02-26 22:45:15 +0530

    body-parser added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Increase request size limit to 50MB (adjust as needed)
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+
   console.log("Application started on http://localhost:" + process.env.APPLICATION_PORT);
 
+  
   await app.listen(process.env.APPLICATION_PORT || 3002);
 }
 bootstrap();
