731446d1a9ea7d879ed81ae1765ec147d544b484
Ham Mim <hammim.in@Hams-MacBook-Pro.local>
2025-03-01 15:08:10 +0530

    font added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // Serve static files
     }),
   ],
   controllers: [AppController],
