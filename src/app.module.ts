b394496c30e28526273de8dfe3b17ab0e5e6a3b4
Ham Mim <hammim.in@Hams-MacBook-Pro.local>
2025-03-03 18:49:41 +0530

    task remove

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,15 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { ScheduleModule } from '@nestjs/schedule';
-import { BlockchainModule } from './blockchain/blockchain.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ScheduleModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'), // Serve static files
     }),
-    BlockchainModule,
   ],
   controllers: [AppController],
   providers: [AppService],
